8f0ad899c3299f6ac80686ab0d813ae2f6272183
Uziel <uzielcivil@gmail.com>
2024-07-09 12:48:47 -0300

    chore: starting the form field

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [LoginLayoutComponent],
+  imports: [LoginLayoutComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+}
