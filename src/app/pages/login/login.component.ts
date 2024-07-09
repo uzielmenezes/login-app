1c640d5338163f61114ab6e650a5d789d90e94b1
Uziel <uzielcivil@gmail.com>
2024-07-09 14:40:06 -0300

    feat: style and functions for the login page / WIP

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+
+interface LoginForm {
+  email: FormControl;
+  password: FormControl;
+}
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [LoginLayoutComponent, ReactiveFormsModule],
+  imports: [LoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
+  loginForm!: FormGroup<LoginForm>;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  submit() {
+    console.log(this.loginForm.value);
+  }
+
+  navigate() {
+    this.router.navigate(['/signup']);
   }
 }
