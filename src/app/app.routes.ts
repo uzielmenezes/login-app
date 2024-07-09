eaea757d2ef834410d7b627ddbbd634e2daf6f73
Uziel <uzielcivil@gmail.com>
2024-07-09 12:37:26 -0300

    feat: login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
