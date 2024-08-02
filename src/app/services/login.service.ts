4cc25d9869d618a487826c6160fa95a5840e2495
Uziel <uzielcivil@gmail.com>
2024-08-02 14:22:25 -0300

    chore: adjustments to integrate with api

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -4,33 +4,39 @@ import { Observable, tap } from 'rxjs';
 
 import { LoginResponse, SignUpResponse } from '../types/login-response.type';
 
+const API_URL = `http://localhost:8080/auth`;
+
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
+  private LOGIN_URL: string = `${API_URL}/login`;
+
+  private SIGNUP_URL: string = `${API_URL}/register`;
+
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>('/login', { email, password }).pipe(
-      tap((value) => {
-        sessionStorage.setItem('auth-token', value.token);
-        sessionStorage.setItem('username', value.email);
-      })
-    );
+    return this.http
+      .post<LoginResponse>(this.LOGIN_URL, { email, password })
+      .pipe(
+        tap((value) => {
+          sessionStorage.setItem('auth-token', value.token);
+          sessionStorage.setItem('username', value.email);
+        })
+      );
   }
 
   signup(
     name: string,
     email: string,
-    password: string,
-    passwordConfirm: string
+    password: string
   ): Observable<SignUpResponse> {
     return this.http
-      .post<SignUpResponse>('/signup', {
+      .post<SignUpResponse>(this.SIGNUP_URL, {
         name,
         email,
         password,
-        passwordConfirm,
       })
       .pipe(
         tap((value) => {
