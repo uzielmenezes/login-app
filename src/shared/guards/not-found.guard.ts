d9bf40498113f9cee6e254e1cb9dc1d07835217f
Uziel <uzielcivil@gmail.com>
2024-08-23 19:01:07 -0300

    chore: update guard

--- src/shared/guards/not-found.guard.ts
+++ src/shared/guards/not-found.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotFoundGuard implements CanActivate {
-  constructor(private toastService: ToastrService) {}
+  constructor(private toastService: ToastrService, private route: Router) {}
 
   canActivate(): boolean {
-    this.toastService.error('The route you tried to access does not exist');
+    const authToken = sessionStorage.getItem('auth-token');
+
+    if (authToken) {
+      this.toastService.error('The route you tried to access does not exist');
+      this.route.navigate(['user']);
+    } else {
+      this.toastService.error('You do not have access');
+    }
+
     return true;
   }
 }
