bb615196d351dabe72be3bd346822ee3b46c853b
Uziel <uzielcivil@gmail.com>
2024-08-23 18:21:58 -0300

    feat: logout function

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  constructor(private router: Router) {}
 
+  logout() {
+    sessionStorage.clear();
+    this.router.navigate(['']);
+  }
 }
